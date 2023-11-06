33d579795cb5a0d5ec70cc70ea435329736381e6
Akhan <93190613+Dulatbay@users.noreply.github.com>
2023-11-06 17:43:29 +0600

    switch to vite js

---
+++ src/pages/RegistrationPage.tsx
@@ -0,0 +1,11 @@
+import {Box} from "@mui/material";
+
+export function RegistrationPage() {
+    return (
+        <Box sx={{
+            position: "absolute"
+        }}>
+
+        </Box>
+    );
+}
